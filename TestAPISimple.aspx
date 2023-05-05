5d9b52353faf3e4b43d0d48089284929fc56a757
Kaito <bryan021212@gmail.com>
2023-05-06 00:09:57 +0800

    Web API Refs added, try in TestAPISimple
    added reference file (WebAPINecessaryControllerClass), necessary things such as async void method and <$ Page Async="true" %> for the page (check TestAPISimple)

---
+++ TestAPISimple.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="TestAPISimple.aspx.cs" Inherits="CountryRoads.TestAPISimple" Async="true" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Test API here Bozo
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+            <br />
+            Result: <br />
+            _______ <br />
+
+            <asp:Label ID="testresult" runat="server">
+
+            </asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ TestAPISimple.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CountryRoads
+{
+    public partial class TestAPISimple : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected async void Button1_Click(object sender, EventArgs e)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://rest-country-api.p.rapidapi.com/"),
+                Headers =
+                {
+                    { "X-RapidAPI-Key", "8f6c6ab0c3mshc7efaf2e55a1070p1f04cajsna153dead5087" },
+                    { "X-RapidAPI-Host", "rest-country-api.p.rapidapi.com" },
+                },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(body);
+                testresult.Text = body;
+            }
+        }
+    }
+}
