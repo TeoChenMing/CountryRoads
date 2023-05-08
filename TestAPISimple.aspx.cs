cdb0b436a1920d218ae0653d3a12a8d45e939205
Aaron <aarontan2002@gmail.com>
2023-05-08 16:00:52 +0800

    API Insert be kinda pog ngl
    Ok so anyways imma leave this message here but i doubt anyone would read it. So what i did is i basically inserted all the countries data into the table and made sure all the empty spaces will be NULL in the database. Have a great day to whoever read this part

--- TestAPISimple.aspx.cs
+++ TestAPISimple.aspx.cs
@@ -5,6 +5,7 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
@@ -103,8 +104,6 @@ namespace CountryRoads
 
                 foreach (var test in jsonObject.Take(5))
                 {
-
-                    
                     //var CountryJoin = string.Join(", ", test.currencies[0].name, test.currencies[0].symbol);
 
                     using (var cmd = new SqlCommand("INSERT INTO country VALUES (@countryId, @countryName, @countryCapital, @countryFlag, @countryArea, @countryPopulation" +
@@ -123,30 +122,8 @@ namespace CountryRoads
                         cmd.Parameters.AddWithValue("@countryTimezone", test.timezones[0]);
                         cmd.Parameters.AddWithValue("@countryCurrency", "TESTING");
 
-                        //Currency test2 = test.currencies;
-                        //Label1.Text += test.area;
-                        //Label1.Text += test2.Keys;
 
-
-
-                        //Currency cur = JsonConvert.DeserializeObject<Currency>(test6);
-                        //Label1.Text += cur.key;
-                        //Label1.Text += test5.symbol;
-
-                        //foreach(test5)
-                        //var currenciesList = "";
-                        //foreach (var currency in test.currencies)
-                        //{ 
-                        //    var countryCurrency = currency.Value;
-                        //    currenciesList += countryCurrency.name;
-                        //}
-                        //Label1.Text = currenciesList;
-
-
-                        //cmd.Parameters.AddWithValue("@continent", "TA");
-                        //cmd.ExecuteNonQuery();
-
-
+                        cmd.ExecuteNonQuery();
                     }
                 };
 
@@ -154,46 +131,112 @@ namespace CountryRoads
 
             }
             //perform add into db
-            
+
         }
 
         protected void Button3_Click(object sender, EventArgs e)
         {
             string readText = File.ReadAllText("C:\\Users\\aaron\\Documents\\GitHub\\CountryRoads\\country.txt");
             var jsonObject = JsonConvert.DeserializeObject<List<test>>(readText);
-            
+            string currencies = "";
 
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["CountryRoadsDB"].ConnectionString);
+            con.Open();
 
             foreach (var test in jsonObject)
             {
                 var currenciesJSON = test.currencies.ToString();
-                if (test.name.common == "Bahamas")
+
+                try
                 {
-                    try
+                    var currencyJSON = JsonConvert.DeserializeObject<Dictionary<String, Object>>(test.currencies.ToString());
+                    foreach (var key in currencyJSON.Values.Take(1))
                     {
-
-                        var currencyJSON = JsonConvert.DeserializeObject<Dictionary<String, Object>>(test.currencies.ToString());
-                        foreach (var key in currencyJSON.Values.Take(1))
+                        string[] currencyDetails = new string[2];
+                        int increment = 0;
+                        var detailsJSON = JsonConvert.DeserializeObject<Dictionary<String, String>>(key.ToString());
+                        foreach (var details in detailsJSON)
                         {
-                            List<String> currencyDetails;
-                            var detailsJSON = JsonConvert.DeserializeObject<Dictionary<String, String>>(key.ToString());
-                            foreach (var details in detailsJSON)
-                            {
-                                Label1.Text += details.Value.ToString();
-                                
-                            }
+                            currencyDetails[increment] = details.Value;
+                            increment++;
                         }
-
-
-
-                    }
-                    catch (Exception ex)
-                    {
-                        Label1.Text = ex.Message;
+                        currencies = String.Join(", ", currencyDetails);
                     }
                 }
-            }
+                catch
+                {
+                    currencies = "NULL";
+                }
 
+                Label1.Text += currencies;
+
+                using (var cmd = new SqlCommand("INSERT INTO country VALUES (@countryId, @countryName, @countryCapital, @countryFlag, @countryArea, @countryPopulation ,@countryLanguage, @countryCurrency, @countryTimezone, NULL)", con))
+                {
+                    cmd.Parameters.AddWithValue("@countryId", test.cca3);
+                    cmd.Parameters.AddWithValue("@countryName", test.name.common);
+                    
+                    try
+                    {
+                        cmd.Parameters.AddWithValue("@countryCapital", test.capital[0] ?? (Object)DBNull.Value);
+                    }
+                    catch
+                    {
+                        cmd.Parameters.AddWithValue("@countryCapital", DBNull.Value);
+                    }
+                    
+                    cmd.Parameters.AddWithValue("@countryFlag", test.flags[0] ?? "");
+                    cmd.Parameters.AddWithValue("@countryArea", test.area);
+                    cmd.Parameters.AddWithValue("@countryPopulation", test.population);
+                    
+                    // LANGUAGE
+                    try
+                    {
+                        if (test.languageNames.Trim().Equals(""))
+                        {
+                            cmd.Parameters.AddWithValue("@countryLanguage", DBNull.Value);
+                        } 
+                        else
+                        {
+                            cmd.Parameters.AddWithValue("@countryLanguage", test.languageNames.Trim() ?? (Object)DBNull.Value);
+                        }
+                    }
+                    catch
+                    {
+                        cmd.Parameters.AddWithValue("@countryLanguage", DBNull.Value);
+                    }
+
+                    // TIMEZONES
+                    try
+                    {
+                        cmd.Parameters.AddWithValue("@countryTimezone", test.timezones[0] ?? (Object)DBNull.Value);
+                    }
+                    catch
+                    {
+                        cmd.Parameters.AddWithValue("@countryTimezone", DBNull.Value);
+                    }
+
+                    // CURRENCY
+                    try
+                    {
+                        if (currencies.Equals("NULL"))
+                        {
+                            cmd.Parameters.AddWithValue("@countryCurrency", DBNull.Value);
+                        }
+                        else
+                        {
+                            cmd.Parameters.AddWithValue("@countryCurrency", currencies ?? (Object)DBNull.Value);
+                        }
+                        
+                    }
+                    catch
+                    {
+                        cmd.Parameters.AddWithValue("@countryCurrency", DBNull.Value);
+                    }
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            con.Close();
         }
     }
 }
