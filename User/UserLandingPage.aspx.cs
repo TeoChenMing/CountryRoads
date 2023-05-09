261efd83ff16f3153790ceddc4e4f869c39aae6a
Aaron <aarontan2002@gmail.com>
2023-05-09 09:50:41 +0800

    Revert "FIxed the Map"
    This reverts commit 2a9324f0c78f57dac2a5e896ac3f988a62a36fab.

--- User/UserLandingPage.aspx.cs
+++ User/UserLandingPage.aspx.cs
@@ -13,19 +13,5 @@ namespace CountryRoads.User
         {
 
         }
-<<<<<<< Updated upstream
-=======
-
-        protected void Path_MouseOver(object sender, EventArgs e)
-        {
-
-        }
-
-        [System.Web.Services.WebMethod]
-        public static string GetCountry(string name)
-        {
-            return "This is your country: " + name;
-        }
->>>>>>> Stashed changes
     }
 }
