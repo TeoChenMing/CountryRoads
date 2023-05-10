ef8e830879f77bb3f34fd4ced9f2359dc5d2db34
Kaito <bryan021212@gmail.com>
2023-05-10 19:28:51 +0800

    Added some pages

---
+++ addDataIntoTables.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CountryRoads
+{
+    public partial class addDataIntoTables : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_addToUsers_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_clearUsers_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_addToQuestions_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_clearQuestions_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
