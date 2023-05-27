09f970badbcaa56d6b4e4b79102e708b6fe16e9e
Aaron <aarontan2002@gmail.com>
2023-05-28 01:55:37 +0800

    k

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -11,7 +11,7 @@ namespace CountryRoads
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            Response.Redirect("~/User/UserLandingPage.aspx");
         }
     }
 }
